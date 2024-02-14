b5caff4b1993a1757f8ed499c99e5195e75d5246
wChoros <twoface1667@gmail.com>
2024-02-15 00:33:13 +0100

    beta responsive menu version

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-function checkMedia(x) {
-    if (x.matches) { // If media query matches
-      document.body.style.backgroundColor = "yellow";
-    } else {
-      document.body.style.backgroundColor = "pink";
-    }
+hamburger = document.querySelector('.hamburger');
+line = document.querySelector('.line');
+line3 = document.querySelector('.line3');
+menu = document.querySelector('.small-menu');
+menuItem = document.querySelectorAll('.small-menu-item');
+header = document.querySelector('header');
+loaded = true;
+hidden = true;
+hamburger.addEventListener('click', () => {
+  line.classList.toggle('line-show');
+  hamburger.classList.toggle('opened');
+  hamburger.setAttribute('aria-expanded', hamburger.classList.contains('opened'));
+  line3.classList.toggle('line-show');
+  menu.classList.toggle('menu-show');
+
+  if (loaded) {
+    loaded = false;
   }
-  
-  // Create a MediaQueryList object
-  var x = window.matchMedia("(max-width: 1200px)")
-  
-  // Attach listener function on state changes
-  x.addEventListener("change", function() {
-    checkMedia(x);
+  else {
+    menu.classList.toggle('menu-hide-animate');
+    header.classList.toggle('header-menu-hide');
+  }
+  if(hidden){
+    hidden = false;
+    menu.classList.toggle('menu-hide');
+  }
+  else{
+    setTimeout(() => {
+      menu.classList.toggle('menu-hide');
+    }, 999);
+    hidden = true;
+  }
+  menuItem.forEach((item) => {
+    item.classList.toggle('small-menu-item-hide');
   });
+
+  header.classList.toggle('header-menu-show');
+
+
+});
